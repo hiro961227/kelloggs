f2feb40efbcc842c97a2a5b89e1efd19c20b6a13
yf0415 <ovrtlen@nvaer.com>
2020-10-23 16:17:57 +0900

    pcup

---
+++ js/qnascroll.js
@@ -0,0 +1,46 @@
+$(function(){ 
+//start   
+    var divAll = $('.page1, .page2, .page3');
+    var secArr = [];
+        
+    divAll.each(function(idx){
+        secArr.push(divAll.eq(idx).offset().top); 
+    });
+
+    $(window).scroll(function(){
+    $.each(secArr,function(idx){
+        if($(window).scrollTop() >= secArr[0]){
+            $('main').css({
+                background:"#232f73",
+                transition:".3s"
+            });
+        }if($(window).scrollTop() >= secArr[1]-200){
+            $('main').css({background:"#333e7c"});
+        }if($(window).scrollTop() >= secArr[2]+100){
+            $('main').css({background:"#455087"});
+        }if($(window).scrollTop() >= secArr[2]*3){
+            $('main').css({background:"#59649a"});
+        }
+    }); 
+    });
+
+
+    //인덱스가안잡혀용..
+    var liAll = $('.page3 ul li p');
+    var popAll = $('.page3 .popup_a');
+    
+    console.log(popAll);
+
+    liAll.hover(function(){
+        idx = $(this).index();
+        console.log(idx);
+        popAll.eq(idx).stop().fadeIn(250);
+    },function(){
+        idx = $(this).index();
+        popAll.eq(idx).stop().fadeOut(250);
+    });
+
+
+    //end  
+});
+    
